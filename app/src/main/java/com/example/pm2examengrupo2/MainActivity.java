36f50401ef17b84d5cec83d5fab90d9dc35df661
Marvin Josue Martinez Zapata <101903394+doguidog@users.noreply.github.com>
2022-11-20 11:05:19 -0600

    Creacion del Crud Create contactos y El Xml de Maps - Marvin Martinez - 20/11/2022

--- app/src/main/java/com/example/pm2examengrupo2/MainActivity.java
+++ app/src/main/java/com/example/pm2examengrupo2/MainActivity.java
@@ -1,14 +1,351 @@
 package com.example.pm2examengrupo2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.pm2examengrupo2.API.Api;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String latitud = "";
+    public static String longitud = "";
+    static final int RESULT_GALLERY_IMG = 200;
+    static final int PETICION_ACCESO_CAM = 100;
+    static final int TAKE_PIC_REQUEST = 101;
+    Bitmap imagen;
+    ImageView foto,btnfoto;
+    Button btnsalvar,btncontactos;
+    EditText txtnombre,txttelefono,txtlatitud,txtlongitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        casteo();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+
+        }
+
+        btnfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sacarPermisos();
+            }
+        });
+
+        btnsalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    validarDatos();
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Advertencia, tomese una foto primero",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btncontactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Activity_Actualizar.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+    public void casteo(){
+        foto = (ImageView) findViewById(R.id.foto);
+        btnfoto = (ImageView) findViewById(R.id.btnfoto);
+        btnsalvar = (Button) findViewById(R.id.btnsalvar);
+        btncontactos = (Button) findViewById(R.id.btncontactos);
+        txtnombre = (EditText) findViewById(R.id.txtnombre);
+        txttelefono = (EditText) findViewById(R.id.txttelefono);
+        txtlatitud = (EditText) findViewById(R.id.txtlatitud);
+        txtlongitud = (EditText) findViewById(R.id.txtlongitud);
+    }
+
+    private void validarDatos() {
+        if(txtnombre.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Escriba su nombre porfavor" ,Toast.LENGTH_LONG).show();
+        }else if (txttelefono.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(), "Escriba su telefono porfavor" ,Toast.LENGTH_LONG).show();
+        }else if (txtlatitud.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(), "Escriba su latitud porfavor" ,Toast.LENGTH_LONG).show();
+        }else if (txtlongitud.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(), "Escriba su longitud porfavor" ,Toast.LENGTH_LONG).show();
+        }
+        else {
+            guardarContacto();
+        }
+    }
+
+    private void limpiar() {
+        txtnombre.setText("");
+        txttelefono.setText("");
+        txtlongitud.setText("");
+        txtlongitud.setText("");
+    }
+
+    //Metodos para configurar la imagen
+
+    private void sacarPermisos() {
+
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},PETICION_ACCESO_CAM);
+        }else{
+            tomarFoto();
+        }
+    }
+
+    private void tomarFoto() {
+        Intent takepic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(takepic.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(takepic,TAKE_PIC_REQUEST);
+        }
+    }
+
+    //***Metodo para convertir imagen***//
+    private String GetStringImage(Bitmap photo) {
+
+        try {
+            ByteArrayOutputStream ba = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 70, ba);
+            byte[] imagebyte = ba.toByteArray();
+            String encode = Base64.encodeToString(imagebyte, Base64.DEFAULT);
+
+            return encode;
+        }catch (Exception ex)
+        {
+            ex.toString();
+        }
+        return "";
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PETICION_ACCESO_CAM)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                tomarFoto();
+            }
+        }else{
+            Toast.makeText(getApplicationContext(),"Se necesitan permisos",Toast.LENGTH_LONG).show();
+        }
+        //permiso para GPS
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                inicioLocalizacion();
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri imageUri;
+        //obtener la iamgen por el almacenamiento interno
+        if(resultCode==RESULT_OK && requestCode==RESULT_GALLERY_IMG)
+        {
+
+            imageUri = data.getData();
+            foto.setImageURI(imageUri);
+            try {
+                imagen=MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
+
+            }catch (Exception e)
+            {
+                Toast.makeText(getApplicationContext(),"Error al seleccionar imagen", Toast.LENGTH_SHORT).show();
+            }
+        }
+        //obtener la iamgen por la camara
+        if(requestCode == TAKE_PIC_REQUEST && resultCode == RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            imagen = (Bitmap) extras.get("data");
+            foto.setImageBitmap(imagen);
+        }
+
+    }
+
+    //Metodo que nos permite inicializar la locacion
+    private void inicioLocalizacion() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setMainActivity(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            //SE VA A LA CONFIGURACION DEL SISTEMA PARA QUE ACTIVE EL GPS UNA VEZ QUE INICIA LA APLICACION
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+
+
+    }
+
+    //Clase que implementa una escucha de la locacion
+    public class Localizacion implements LocationListener {
+        MainActivity mainActivity;
+
+        public void setMainActivity(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+            // debido a la deteccion de un cambio de ubicacion
+
+            loc.getLatitude();
+            loc.getLongitude();
+
+            String Text = "Mi ubicacion actual es: " + "\n Lat = "
+                    + loc.getLatitude() + "\n Long = " + loc.getLongitude();
+
+
+            MainActivity.setLatitud(loc.getLatitude()+"");
+            MainActivity.setLongitud(loc.getLongitude()+"");
+            txtlatitud.setText(loc.getLatitude()+"");
+            txtlongitud.setText(loc.getLongitude()+"");
+            this.mainActivity.setLocation(loc);
+        }
+
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
+        }
+    }
+
+    public static void setLatitud(String latitud) {
+        MainActivity.latitud = latitud;
+    }
+
+    public static void setLongitud(String longitud) {
+        MainActivity.longitud = longitud;
+    }
+
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //Metodo que crea un contacto y lo guarda en la BD
+
+    private void guardarContacto() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        HashMap<String, String> parametros = new HashMap<>();
+
+        String fotoString = GetStringImage(imagen);
+
+
+        //setear los parametros mediante put
+        parametros.put("nombre", txtnombre.getText().toString());
+        parametros.put("telefono", txttelefono.getText().toString());
+        parametros.put("latitud", txtlatitud.getText().toString());
+        parametros.put("longitud", txtlongitud.getText().toString());
+        parametros.put("foto", fotoString);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, Api.EndPointCreateUsuario,
+                new JSONObject(parametros), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Toast.makeText(getApplicationContext(), "String Response " + response.getString("mensaje").toString(), Toast.LENGTH_SHORT).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+
+        limpiar();
+    }
+
+
 }
